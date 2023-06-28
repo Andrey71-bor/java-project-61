93779f58506d3770386dcdc665e5df4ee5b15d5a
Andrey <bortnikov.ae71@gmail.com>
2023-06-28 12:05:21 +0300

    Add Cli fnd update App

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
+}
